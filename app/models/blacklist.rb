7119261711be3d49813c97fd2b2a16ffe7ce209d
himanushi <himanushi@users.noreply.github.com>
2017-07-03 23:15:28 +0900

    半角カナを全角カナに変換してからNGワードを置換する

--- app/models/blacklist.rb
+++ app/models/blacklist.rb
@@ -1,6 +1,9 @@
+require 'nkf'
+
 class Blacklist < ApplicationRecord
   class << self
     def parse(message)
+      message = NKF.nkf('-Xw',message)
       self.all.each do |black|
         message.gsub!(/#{black.word}/, '*' * black.word.size)
       end
