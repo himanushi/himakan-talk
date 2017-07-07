beeda0bbd340ba38183689ddacee87acd68612e7
himanushi <himanushi@users.noreply.github.com>
2017-07-08 03:05:02 +0900

    メッセージ内のURLにリンクをつけるようにする

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
+require "uri"
+
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
@@ -9,6 +11,15 @@ class Message < ApplicationRecord
   end
 
   def escape_content
-    ERB::Util.html_escape(content)
+    text = ERB::Util.html_escape(content)
+
+    URI.extract(text, ['http','https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      text.gsub!(url, sub_text)
+    end
+
+    text
   end
 end
